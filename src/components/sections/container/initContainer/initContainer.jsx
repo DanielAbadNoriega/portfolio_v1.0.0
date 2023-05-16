9013e2275bc25ba83ccf15cc099d6508c2b7231c
Daniel Abad Noriega <daniel.abad.noriega@gmail.com>
2023-05-16 20:33:38 +0200

    Init modified

--- src/components/sections/container/initContainer/initContainer.jsx
+++ src/components/sections/container/initContainer/initContainer.jsx
@@ -5,6 +5,7 @@ import logo_250 from "../../../../assets/imgs/logo/Logo_250px.png";
 import logo_500 from "../../../../assets/imgs/logo/Logo_500px.png";
 
 const InitContainer = () => {
+
   const navigate = useNavigate();
 
   const [isMobile, setisMobile] = useState(false);
@@ -16,8 +17,12 @@ const InitContainer = () => {
     }, 10000);
   };
 
+  // TODO: tapAnyWhere
+  // TODO: fadeIn
+
   return (
     <div className="initContainer" onClick={() => tapAnyWhere()}>
+      {/* TITLE */}
       <div className="wraper" onClick={() => tapAnyWhere()}>
         {/* CONTACT */}
         <div className="box">
@@ -33,7 +38,7 @@ const InitContainer = () => {
                 className="sides side-2"
                 onClick={() => navigate("/contact")}
               >
-                <h1>CONTACT.</h1>
+                <h1>CONTACT</h1>
               </div>
             </div>
           </div>
@@ -46,7 +51,7 @@ const InitContainer = () => {
                 <h1>I'M</h1>
               </div>
               <div className="sides side-2" onClick={() => navigate("/about")}>
-                <h1>ABOUT.</h1>
+                <h1>ABOUT</h1>
               </div>
             </div>
           </div>
@@ -57,19 +62,19 @@ const InitContainer = () => {
             <div className="box-content">
               <div className="sides side-1" onClick={() => navigate("/skills")}>
                 <h1>
-                  <span>DANIEL.</span>
+                  <span>DANIEL</span>
                 </h1>
               </div>
               <div className="sides side-2" onClick={() => navigate("/skills")}>
                 <h1>
-                  <span>SKILLS.</span>
+                  <span>SKILLS</span>
                 </h1>
               </div>
             </div>
           </div>
         </div>
       </div>
-
+      {/* LOGO */}
       <img
         src={logo_250}
         srcSet={`
@@ -83,53 +88,6 @@ const InitContainer = () => {
         alt="Logo"
         onClick={() => tapAnyWhere()}
       />
-
-      {/* <div id="init-section">
-        <h1>
-          {" "}
-          <br />
-          <span
-            role="button"
-            onClick={() => navigate("/contact")}
-            className="init-section-contact"
-            style={{ cursor: "pointer" }}
-          >
-            HI.
-          </span>
-          <br />
-          <span
-            role="button"
-            onClick={() => navigate("/about")}
-            className="init-section-about"
-            style={{ cursor: "pointer" }}
-          >
-            I'M,
-          </span>
-          <br />
-          <span
-            role="button"
-            onClick={() => navigate("/skills")}
-            className="init-section-skills"
-            style={{ cursor: "pointer" }}
-          >
-            DANIEL
-          </span>
-          <br />
-        </h1>
-
-        <img
-          src={logo_250}
-          srcSet={`
-            ${logo_250} 250w,
-            ${logo_500} 500w
-          `}
-          sizes="(min-width: 1500px) 500px,
-          (min-width: 800px) 400px,
-          (max-width: 800px) 250px,
-           400px"
-          alt="Logo"
-        />
-      </div> */}
     </div>
   );
 };
