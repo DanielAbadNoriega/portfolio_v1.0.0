de064531647994f3b24b0a44dd5b869c354a0706
Daniel Abad Noriega <daniel.abad.noriega@gmail.com>
2023-05-24 20:33:17 +0200

    Init screen-laptop modified

--- src/components/sections/container/initContainer/initContainer.jsx
+++ src/components/sections/container/initContainer/initContainer.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { IoIosArrowUp } from "react-icons/io";
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { IoIosArrowUp } from 'react-icons/io';
 
-import logo_250 from "../../../../assets/imgs/logo/Logo_250px.png";
-import logo_500 from "../../../../assets/imgs/logo/Logo_500px.png";
+import logo_250 from '../../../../assets/imgs/logo/Logo_250px.png';
+import logo_500 from '../../../../assets/imgs/logo/Logo_500px.png';
 
 const InitContainer = () => {
   const [boxMove, setBoxMove] = useState(false);
@@ -13,16 +13,16 @@ const InitContainer = () => {
   useEffect(() => {
     const originPos = () => {
       setCounter(1);
-      console.log("[ tapAnyWhere - TIMEOUT ] Entro.");
+      // console.log("[ tapAnyWhere - TIMEOUT ] Entro.");
       setTimeout(() => {
         setIsMoved(false);
         setCounter(0);
-        console.log("[ tapAnyWhere - TIMEOUT ] Box move: ", boxMove);
+        // console.log("[ tapAnyWhere - TIMEOUT ] Box move: ", boxMove);
       }, 10000);
     };
 
-    console.log("[ tapAnyWhere - UseEffect ] box move: ", boxMove);
-    console.log("[ tapAnyWhere - UseEffect ] box moved: ", isMoved);
+    /* console.log("[ tapAnyWhere - UseEffect ] box move: ", boxMove);
+    console.log("[ tapAnyWhere - UseEffect ] box moved: ", isMoved); */
     return () => {
       if (isMoved && counter === 0) originPos();
     };
@@ -30,89 +30,101 @@ const InitContainer = () => {
 
   const navigate = useNavigate();
 
-  // TODO: tapAnyWhere
-
   const tapAnyWhere = () => {
     setBoxMove(!boxMove);
     setIsMoved(true);
   };
 
   const boxMoveStyle = {
-    transform: "translateZ(-150px) rotateX(90deg)",
+    transform: 'translateZ(-150px) rotateX(90deg)',
   };
 
   return (
     <div className="initContainer" onClick={() => tapAnyWhere()}>
-      {/* LOGO */}
-      <img
-        src={logo_250}
-        srcSet={`
+      <div className='initContainer-main'>
+        {/* LOGO */}
+        <img
+          src={logo_250}
+          srcSet={`
             ${logo_250} 250w,
             ${logo_500} 500w
           `}
-        sizes="(min-width: 1500px) 500px,
+          sizes="(min-width: 1500px) 500px,
           (min-width: 800px) 400px,
           (max-width: 800px) 250px,
            400px"
-        alt="Logo"
-      />
+          alt="Logo"
+        />
 
-      {/* TITLE */}
-      <div className="wraper">
-        {/* CONTACT */}
-        <div className="box">
-          <div className="single-box">
-            <div
-              className="box-content"
-              style={boxMove && isMoved ? boxMoveStyle : {}}
-            >
+        {/* TITLE */}
+        <div className="wraper">
+          {/* CONTACT */}
+          <div className="box">
+            <div className="single-box">
               <div
-                className="sides side-1"
-                onClick={() => navigate("/contact")}
+                className="box-content"
+                style={boxMove && isMoved ? boxMoveStyle : {}}
               >
-                <h1>HI.</h1>
-              </div>
-              <div
-                className="sides side-2"
-                onClick={() => navigate("/contact")}
-              >
-                <h1>CONTACT</h1>
+                <div
+                  className="sides side-1"
+                  onClick={() => navigate('/contact')}
+                >
+                  <h1>HI.</h1>
+                </div>
+                <div
+                  className="sides side-2"
+                  onClick={() => navigate('/contact')}
+                >
+                  <h1>CONTACT</h1>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        {/* ABOUT */}
-        <div className="box">
-          <div className="single-box">
-            <div
-              className="box-content"
-              style={boxMove && isMoved ? boxMoveStyle : {}}
-            >
-              <div className="sides side-1" onClick={() => navigate("/about")}>
-                <h1>I'M</h1>
-              </div>
-              <div className="sides side-2" onClick={() => navigate("/about")}>
-                <h1>ABOUT</h1>
+          {/* ABOUT */}
+          <div className="box">
+            <div className="single-box">
+              <div
+                className="box-content"
+                style={boxMove && isMoved ? boxMoveStyle : {}}
+              >
+                <div
+                  className="sides side-1"
+                  onClick={() => navigate('/about')}
+                >
+                  <h1>I'M</h1>
+                </div>
+                <div
+                  className="sides side-2"
+                  onClick={() => navigate('/about')}
+                >
+                  <h1>ABOUT</h1>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        {/* SKILLS */}
-        <div className="box">
-          <div className="single-box">
-            <div
-              className="box-content"
-              style={boxMove && isMoved ? boxMoveStyle : {}}
-            >
-              <div className="sides side-1" onClick={() => navigate("/skills")}>
-                <h1>
-                  <span>DANIEL</span>
-                </h1>
-              </div>
-              <div className="sides side-2" onClick={() => navigate("/skills")}>
-                <h1>
-                  <span>SKILLS</span>
-                </h1>
+          {/* SKILLS */}
+          <div className="box">
+            <div className="single-box">
+              <div
+                className="box-content"
+                style={boxMove && isMoved ? boxMoveStyle : {}}
+              >
+                <div
+                  className="sides side-1"
+                  onClick={() => navigate('/skills')}
+                >
+                  <h1>
+                    <span>DANIEL</span>
+                  </h1>
+                </div>
+                <div
+                  className="sides side-2"
+                  onClick={() => navigate('/skills')}
+                >
+                  <h1>
+                    <span>SKILLS</span>
+                  </h1>
+                </div>
               </div>
             </div>
           </div>
